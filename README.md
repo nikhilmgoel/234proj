d57c99af91058e02184d3f94bdfbca510b49bc70
Nikhil Goel <nikhilusa@gmail.com>
2018-03-09 16:26:25 -0800

    guuci

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # 234proj
 CS 234 Project: Navigating Pedestrian Spaces Using Public Intersection Data
-
-https://www.overleaf.com/14055235bmhzqpgdykjx#/54494021/
--- gym-navigate/gym_navigate/envs/navigate_env.py
+++ gym-navigate/gym_navigate/envs/navigate_env.py
@@ -1,11 +1,14 @@
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
+import math
+import numpy as np
 
 class NavigateEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self):
+  	
   	return
     
   def step(self, action):
